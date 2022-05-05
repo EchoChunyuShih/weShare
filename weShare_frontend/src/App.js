187355e9c1d1cc6addcc17692512b12d7c7c55e8
Echo Shih <chunyushih@outlook.com>
2022-05-06 03:24:27 +0800

    [feat] App routes

--- weShare_frontend/src/App.js
+++ weShare_frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Home from "./pages/Home";
+import Home from "./containers/Home";
 
 const App = () => {
   return (
